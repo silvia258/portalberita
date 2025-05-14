467bda03913aff7b1c361a22a1895ee44403f6bb
silvia258 <silviadwic258@gmail.com>
2025-05-14 17:16:01 +0700

    tambah post

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Title'),
+
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Slug')
+                    ->unique(Post::class, 'slug'),
+
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required()
+                    ->label('Category'),
+
+                Forms\Components\RichEditor::make('content')
+                    ->required()
+                    ->columnSpanFull()
+                    ->label('Content'),
+
+                Forms\Components\FileUpload::make('image')
+                    ->required()
+                    ->image()
+                    ->imageEditor()
+                    ->label('Image URL'),
+
+                Forms\Components\TextInput::make('author')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Author'),
+
+                Forms\Components\DateTimePicker::make('date')
+                    ->required()
+                    ->label('Date')
+                    ->default(now()),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Title'),
+
+                Tables\Columns\TextColumn::make('category.name')
+                    ->label('Category'),
+
+                Tables\Columns\TextColumn::make('author')
+                    ->label('Author'),
+
+                Tables\Columns\TextColumn::make('date')
+                    ->label('Date')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
